1fa1d95345b6b25cad2a0afcf7d321360bd5279e
Timo Wendt <timo@tjwendt.de>
2013-07-08 09:05:38 -0300

    Irda: Add Irda System Service for Samsung devices
    Add Irda System Service that is compatible with the Samsung devices
    .
    This is not a perfect solution yet, but it does work and should therefore be a starting point.
    I do not know how to make this code exclusive to the S4, maybe someone can help me out with that.
    
    PatchSet 2: Now the Irda service is not started by default. It can be enabled via overlay.
    
    PatchSet 3: Rebase
    
    PatchSet 4: It now uses a HAL module to communicate with sysfs. However it currently
    fails when loading the module
    
    PatchSet 5: Corrected a typo in a comment
    
    PatchSet 6: Cleanup
    
    PatchSet 7: Renamed native method to send_ircode_native
    
    Patchset 8: Renamed commit message to indicate general applicability to Samsung devices
    including n80xx.  Need to determine how to handle HTC and Sony...  Also added logging
    when module load fails or device opening fails.
    
    PatchSet 9: Fix missing semicolon causing breakage in the build
    
    Change-Id: I54535d95a7b3c060fcefcf81b97d3d23bf4ff279

--- core/java/android/app/ContextImpl.java
+++ core/java/android/app/ContextImpl.java
@@ -91,6 +91,8 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.IPowerManager;
 import android.os.IUserManager;
+import android.hardware.IIrdaManager;	
+import android.hardware.IrdaManager;
 import android.os.Looper;
 import android.os.PowerManager;
 import android.os.Process;
@@ -573,6 +575,12 @@ class ContextImpl extends Context {
                     IFmTransmitter service = IFmTransmitter.Stub.asInterface(b);
                     return new FmTransmitterImpl(service);
                 }});
+	registerService(IRDA_SERVICE, new StaticServiceFetcher() {	
+			public Object createStaticService() {	
+			IBinder b = ServiceManager.getService(IRDA_SERVICE);	
+			IIrdaManager service = IIrdaManager.Stub.asInterface(b);	
+			return new IrdaManager(service);	
+			}});
     }
 
     static ContextImpl getImpl(Context context) {
